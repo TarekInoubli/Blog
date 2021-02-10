c2092ee8da4141dfaa5c364af53c3e521021c7fd
Tarek Inoubli <tarek.inoubli@zeiss.com>
2021-02-10 15:22:26 +0100

    Add BlogPost data model

---
+++ BlazorBlog/BlazorBlog/Shared/BlogPost.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace BlazorBlog.Shared
+{
+    /// <summary>
+    /// BlogPost model.
+    /// </summary>
+    public class BlogPost
+    {
+        /// <summary>
+        /// The post id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The post title
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The post content
+        /// </summary>
+        public string Content { get; set; }
+
+        /// <summary>
+        /// The post description
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// The post author
+        /// </summary>
+        public string Author { get; set; }
+
+        /// <summary>
+        /// The post datetime
+        /// </summary>
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+
+        /// <summary>
+        /// Is the post already published?
+        /// </summary>
+        public bool IsPublished { get; set; } = true;
+
+        /// <summary>
+        /// Is the post already deleted?
+        /// </summary>
+        public bool IsDeleted { get; set; } = false;
+    }
+}
